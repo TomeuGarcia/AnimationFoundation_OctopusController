2b39e741ba9adb75ce2fa84762e6d679156a2a0a
Tomeu Garcia <tomeu.garcia@enti.cat>
2022-11-10 11:14:53 +0100

    Started CCD

--- OctopusController/MyOctopusController.cs
+++ OctopusController/MyOctopusController.cs
@@ -23,6 +23,12 @@ namespace OctopusController
         float _twistMin, _twistMax;
         float _swingMin, _swingMax;
 
+
+        float[] _theta;
+        float[] _sin;
+        float[] _cos;
+
+
         #region public methods
         //DO NOT CHANGE THE PUBLIC METHODS!!
 
@@ -57,6 +63,9 @@ namespace OctopusController
             _randomTargets = randomTargets;
             //TODO: use the regions however you need to make sure each tentacle stays in its region
 
+            _theta = new float[_tentacles[0].Bones.Length];
+            _sin = new float[_tentacles[0].Bones.Length];
+            _cos = new float[_tentacles[0].Bones.Length];
         }
 
               
@@ -87,8 +96,60 @@ namespace OctopusController
         #region private and internal methods
         //todo: add here anything that you need
 
-        void update_ccd() {
-           
+        void update_ccd() 
+        {
+            for (int tentacleI = 0; tentacleI < _tentacles.Length; ++tentacleI)
+            {              
+
+                for (int i = _tentacles[tentacleI].Bones.Length - 2; i >= 0; i--)
+                {
+                    // The vector from the ith joint to the end effector
+                    Vector3 r1 = (_tentacles[tentacleI].Bones[_tentacles[tentacleI].Bones.Length - 1].transform.position -
+                        _tentacles[tentacleI].Bones[i].transform.position).normalized;
+
+                    // The vector from the ith joint to the target
+                    Vector3 r2 = (_randomTargets[tentacleI].position - _tentacles[tentacleI].Bones[i].transform.position).normalized;
+
+                    // to avoid dividing by tiny numbers
+                    if (r1.magnitude * r2.magnitude <= 0.001f)
+                    {
+                        // cos ? sin? 
+                        _cos[i] = 1.0f;
+                        _sin[i] = 0.0f;
+                    }
+                    else
+                    {
+                        // find the components using dot and cross product
+                        //TODO4
+                        float dot = Vector3.Dot(r1, r2);
+                        _cos[i] = dot;
+                        Vector3 cross = Vector3.Cross(r1, r2);
+                        _sin[i] = cross.magnitude;
+                    }
+
+
+                    // The axis of rotation 
+                    Vector3 axis = Vector3.Cross(r1, r2).normalized;
+
+                    // find the angle between r1 and r2 (and clamp values if needed avoid errors)
+                    _theta[i] = Mathf.Acos(Mathf.Clamp(_cos[i], -1f, 1f));
+
+                    //Optional. correct angles if needed, depending on angles invert angle if sin component is negative
+                    if (_sin[i] < 0.0f)
+                        _theta[i] = -_theta[i];
+
+
+                    // obtain an angle value between -pi and pi, and then convert to degrees
+                    _theta[i] = _theta[i] * Mathf.Rad2Deg;
+
+                    // rotate the ith joint along the axis by theta degrees in the world space.
+                    _tentacles[tentacleI].Bones[i].transform.rotation = 
+                        Quaternion.AngleAxis(_theta[i], axis) * _tentacles[tentacleI].Bones[i].transform.rotation;
+
+                }
+            }
+            
+            
 
         }
 
