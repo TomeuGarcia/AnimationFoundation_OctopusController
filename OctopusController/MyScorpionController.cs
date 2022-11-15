7256436a9f810dca20d39a599e0f25ea01d5f3de
TomeuGarcia <tomeu.garcia@enti.cat>
2022-11-15 13:31:56 +0100

    Started GradientDescent

--- OctopusController/MyScorpionController.cs
+++ OctopusController/MyScorpionController.cs
@@ -10,11 +10,47 @@ namespace OctopusController
   
     public class MyScorpionController
     {
+        public struct PositionRotation
+        {
+            Vector3 position;
+            Quaternion rotation;
+
+            public PositionRotation(Vector3 position, Quaternion rotation)
+            {
+                this.position = position;
+                this.rotation = rotation;
+            }
+
+            // PositionRotation to Vector3
+            public static implicit operator Vector3(PositionRotation pr)
+            {
+                return pr.position;
+            }
+            // PositionRotation to Quaternion
+            public static implicit operator Quaternion(PositionRotation pr)
+            {
+                return pr.rotation;
+            }
+        }
+
         //TAIL
         Transform tailTarget;
         Transform tailEndEffector;
         MyTentacleController _tail;
         float animationRange;
+
+        float[] _tailBoneAngles;
+        Vector3[] _tailBoneAxis;
+
+        public delegate float ErrorFunction(Vector3 target, float[] solution);
+        private ErrorFunction _errorFunction;
+
+        public float DeltaGradient = 0.1f; // Used to simulate gradient (degrees)
+        public float LearningRate = 0.1f; // How much we move depending on the gradient
+
+        public float StopThreshold = 0.1f; // If closer than this, it stops
+        public float SlowdownThreshold = 0.25f; // If closer than this, it linearly slows down
+
 
         //LEGS
         Transform[] legTargets;
@@ -40,7 +76,17 @@ namespace OctopusController
         {
             _tail = new MyTentacleController();
             _tail.LoadTentacleJoints(TailBase, TentacleMode.TAIL);
+
             //TODO: Initialize anything needed for the Gradient Descent implementation
+
+            _tailBoneAxis = new Vector3[_tail.Bones.Length];
+            _tailBoneAngles = new float[_tail.Bones.Length];
+            for (int i = 0; i < _tail.Bones.Length; ++i)
+            {
+                _tail.Bones[i].localRotation.ToAngleAxis(out _tailBoneAngles[i], out _tailBoneAxis[i]);
+            }
+
+            _errorFunction = DistanceFromTarget;
         }
 
         //TODO: Check when to start the animation towards target and implement Gradient Descent method to move the joints.
@@ -71,16 +117,92 @@ namespace OctopusController
             //check for the distance to the futureBase, then if it's too far away start moving the leg towards the future base position
             //
         }
-        //TODO: implement Gradient Descent method to move tail if necessary
-        private void updateTail()
-        {
-
-        }
+        
         //TODO: implement fabrik method to move legs 
         private void updateLegs()
         {
 
         }
+
+        //TODO: implement Gradient Descent method to move tail if necessary
+        private void updateTail()
+        {
+
+        }
+
         #endregion
+
+
+
+        // Gradient Descent functions
+        public void ApproachTarget(Vector3 target)
+        {
+            //TODO (done)
+            // Update rotations
+            for (int i = 0; i < _tailBoneAngles.Length; ++i)
+            {
+                _tailBoneAngles[i] = _tailBoneAngles[i] - (LearningRate * CalculateGradient(target, _tailBoneAngles, i, DeltaGradient));
+            }
+
+            // TODO !!!!!!!!
+            // Aplly rotations
+            //for (int i = 0; i < _tailBoneAngles.Length; i++)
+            //{
+            //    Joints[i].MoveArm(Solution[i]);
+            //}
+        }
+
+
+        public float CalculateGradient(Vector3 target, float[] Solution, int i, float delta)
+        {
+            //TODO
+            // (Tomeu: I don't even know if this does what it is supposed to do)
+
+            Solution[i] += delta; // Temporaraly get delta solution
+            float deltaDistamceFromTarget = _errorFunction(target, Solution);
+
+            Solution[i] -= delta; // Reset Solution
+
+            return (deltaDistamceFromTarget - _errorFunction(target, Solution)) / delta;
+        }
+
+        // Returns the distance from the target, given a solution
+        public float DistanceFromTarget(Vector3 target, float[] Solution)
+        {
+            Vector3 point = ForwardKinematics(Solution);
+            return Vector3.Distance(point, target);
+        }
+
+
+        /* Simulates the forward kinematics,
+         * given a solution. */
+
+
+        public PositionRotation ForwardKinematics(float[] Solution)
+        {
+            Vector3 prevPoint = _tail.Bones[0].transform.position;
+
+            // Takes object initial rotation into account
+            //Quaternion rotation = transform.rotation;
+            Quaternion rotation = Quaternion.identity; // ??? ask if correct
+
+            //TODO (done)
+            for (int i = 0; i < Solution.Length - 1; ++i)
+            {
+                Vector3 prev = prevPoint;
+
+                rotation = rotation * Quaternion.AngleAxis(Solution[i], _tailBoneAxis[i]);
+
+                //prevPoint += rotation * Joints[i + 1].StartOffset;                            TODO: compute offsets!
+
+                Debug.DrawLine(prevPoint, prev, Color.blue);
+            }
+
+
+            // The end of the effector
+            return new PositionRotation(prevPoint, rotation);
+        }
+
+
     }
 }
